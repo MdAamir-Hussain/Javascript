b9b1951f8fd377a2366c69965bc2975d9e877143
Md Aamir Hussain <mdaamirhussain319@gmail.com>
2023-09-02 14:02:04 +0530

    map

--- 01/loop.js
+++ 01/loop.js
@@ -1,4 +1,12 @@
 const arr = [1,2,3,4,5]
 for (const num of arr) {
-    console.log(num);
+    // console.log(num);
 }
+
+const map = new Map()
+
+map.set('IN','India')
+map.set('USA', 'United States Of America')
+map.set('FR', 'France')
+
+console.log(map);
