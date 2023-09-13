7c028e84f5d590a438118aff375b106e70ac82c5
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2023-09-13 10:40:12 +0530

    summ-arr-num

--- 02/summ.js
+++ 02/summ.js
@@ -1,8 +1,18 @@
-var arr = [1,2,3,4,5,6,7]
+// var arr = [1,2,3,4,5,6,7]
 
+// var sum =0
+
+// arr.forEach(function (elem){
+//     sum  = sum +elem
+// })
+// console.log(sum);
+
+var arr = ['heeheheh', 12432, 'abklha', 4,'foollsls']
 var sum =0
 
 arr.forEach(function (elem){
-    sum  = sum +elem
+    if(typeof elem ==='number'){
+        sum  = sum +elem
+    }
 })
 console.log(sum);
