b407bd3ef63f380a0629347fb988739b1976e412
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2023-09-13 09:36:57 +0530

    rev-alt

--- 02/revs.js
+++ 02/revs.js
@@ -1,5 +1,17 @@
-const a = (x) =>{
-    return Number(x.toString().split("").reverse().join(""))
+// const a = (x) =>{
+//     return Number(x.toString().split("").reverse().join(""))
+// }
+
+// console.log(a(3433));
+
+const y = (num) => {
+    var rev =0
+    while (num>0) {
+        var rem = num%10
+        rev = rev*10+rem
+        num  = Math.floor(num/10)
+    }
+    return rev
 }
 
-console.log(a(3433));
+console.log(y(458984931));
